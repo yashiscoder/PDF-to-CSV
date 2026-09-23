aead3e97767473c567a7e65fe130bb954542db59
Yash Kumawat <kumawatyash060@gmail.com>
2026-09-23 16:12:31 +0530

    register in auth

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["Authentication"]
-)
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+class UserRegister(BaseModel):
+    name: str
+    email: str
+    password_hash: str
+
+@router.post("/register")
+def register_user(name: UserRegister):
+    return {"message": f"User {name.username} created via APIRouter!"}
