9d720f1d64f4813e55f673374f4af6910d935a2d
Yash Kumawat <kumawatyash060@gmail.com>
2026-09-24 12:34:00 +0530

    injecting into db

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from app.database.connection import get_db
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
-
 
 class UserRegister(BaseModel):
     name: str
@@ -14,7 +15,10 @@ class UserRegister(BaseModel):
 
 
 @router.post("/register")
-def register_user(user: UserRegister):
+def register_user(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
     return {
         "message": f"User {user.name} created via APIRouter!"
     }
