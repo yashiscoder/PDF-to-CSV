31e8642fe8d5764ca98a5d85124448c1810e9714
Yash Kumawat <kumawatyash060@gmail.com>
2026-09-24 15:49:00 +0530

    updates

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
+from app.database.models import User
+
 
 router = APIRouter(
     prefix="/auth",
@@ -13,12 +15,21 @@ class UserRegister(BaseModel):
     email: str
     password: str
 
-
 @router.post("/register")
 def register_user(
     user: UserRegister,
     db: Session = Depends(get_db)
 ):
+    duplicate_email = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+    if duplicate_email:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered."
+        )
     return {
         "message": f"User {user.name} created via APIRouter!"
     }
